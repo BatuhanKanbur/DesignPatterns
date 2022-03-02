e8ac754764939ba1fdf79918dd58d160d5d19315
BatuhanKanbur <batuhankanbur95@gmail.com>
2022-03-02 03:21:04 +0300

    A

---
+++ Assets/Other/BlueprintSaveLoad.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueprintSaveLoad : MonoBehaviour
+{
+    [Serializable]
+    public class BlueprintObject
+    {
+        public string objectName;
+        public Vector3 objectPos;
+        public Vector3 objectRot;
+        public string transformParent;
+    }
+    [Serializable]
+    public class BlueprintObjectsList
+    {
+        public List<BlueprintObject> blueprintObjects;
+    }
+    public BlueprintObjectsList blueprintList;
+    public BlueprintObjectsList blueprintList1;
+    void Start()
+    {
+        MeshRenderer[] _gos = FindObjectsOfType<MeshRenderer>();
+        print(_gos.Length);
+        print("aaa");
+        foreach (var _go in _gos)
+        {
+            BlueprintObject _newObj = new BlueprintObject();
+            _newObj.objectName = _go.name;
+            _newObj.objectPos = _go.transform.position;
+            _newObj.objectRot = _go.transform.localEulerAngles;
+            _newObj.transformParent = GetGameObjectPath(_go.transform);
+            blueprintList.blueprintObjects.Add(_newObj);
+        }
+        string _jsonStr=(JsonUtility.ToJson(blueprintList));
+        LoadJson(_jsonStr);
+      //  blueprintList.blueprintObjects.Clear();
+    }
+    public void LoadJson(string _str)
+    {
+        blueprintList1 = JsonUtility.FromJson<BlueprintObjectsList>(_str);
+    }
+    public Vector3 GetJsonVector3(string _vector3Str)
+    {
+        return Vector3.zero;
+    }
+    public float ConvertFloat(string _floatStr)
+    {
+        return float.Parse(_floatStr);
+    }
+    private string GetGameObjectPath(Transform transform)
+    {
+        string path = "";
+        while (transform.parent != null)
+        {
+            transform = transform.parent;
+            path = transform.name + "/" + path;
+        }
+        return path;
+    }
+
+}
